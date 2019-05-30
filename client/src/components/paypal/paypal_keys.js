1d40d2a265a91a041840d8a1d7cf4955a368682a
matt ruggiero <matt@mattruggiero.com>
2019-05-29 21:38:16 -0700

    keys

--- client/src/components/paypal/paypal_keys.js
+++ client/src/components/paypal/paypal_keys.js
@@ -1,13 +1,21 @@
 
+// let paypalKeys;
+// if(process.env.NODE_ENV === 'production'){
+//      paypalKeys = require('./paypal_keys_prod').KEYS_PROD;
+// }else{
+//      paypalKeys = require('./paypal_keys_dev').KEYS_DEV;
+// }
+
+// export const CLIENT = paypalKeys;
+
+
+
+
 let paypalKeys;
 if(process.env.NODE_ENV === 'production'){
-     paypalKeys = require('./paypal_keys_prod').KEYS_PROD;
+     paypalKeys = require('./paypal_keys_dev').KEYS_DEV;
 }else{
      paypalKeys = require('./paypal_keys_dev').KEYS_DEV;
 }
 
 export const CLIENT = paypalKeys;
-
-
-
-
