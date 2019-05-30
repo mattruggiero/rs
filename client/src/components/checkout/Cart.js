1c00560e9fd4a1179bff47ab733c55fa85b7c366
matt ruggiero <matt@mattruggiero.com>
2019-05-29 22:14:00 -0700

    fix

--- client/src/components/checkout/Cart.js
+++ client/src/components/checkout/Cart.js
@@ -9,7 +9,8 @@ import axios from 'axios';
 import { emptyCart } from '../../actions/cartActions';
 
 // won't work in PaypalButton.js 
-import { CLIENT } from '../paypal/paypal_keys';
+//import { CLIENT } from '../paypal/paypal_keys';
+const CLIENT = process.env.PAYPAL_PRODUCTION_KEY;
 
 function removePurchasedItemsFromInventory(cart){
     axios({method:'post',url:'/removeRecords',data:cart})
